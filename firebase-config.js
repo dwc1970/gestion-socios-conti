5b6435fcec9acddbb62eeed08fe294d2b03f892a
dwc1970 <dariocarrizo32@gmail.com>
2026-02-10 15:54:24 -0300

    Version estable: conexion firebase y html lista

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
+// Configuración de Firebase - Club Constitución
 const firebaseConfig = {
-  apiKey: "AIzaSyBEszqWYoGEk4vOk4MEhf9k73qFCI8xtis",
+  apiKey: "AIzaSyBEszqWYoGEk4v0k4MEHf9k73qFCI8xtis",
   authDomain: "club-constitucion.firebaseapp.com",
   projectId: "club-constitucion",
   storageBucket: "club-constitucion.firebasestorage.app",
   messagingSenderId: "882017273470",
   appId: "1:882017273470:web:1487a980977a2cfb76ba2f",
-  measurementId: "G-2R18JY54ZZ"
+  measurementId: "G-2D1BJV5X7Z"
 };
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
